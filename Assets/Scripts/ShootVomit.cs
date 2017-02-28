79a04bfcf9077a30adcc6d7d1a266aff02d3e027
Unknown <tcf234@nyu.edu>
2017-02-27 19:38:00 -0500

    passenger model prefab; edited vomit location and size

--- Assets/Scripts/ShootVomit.cs
+++ Assets/Scripts/ShootVomit.cs
@@ -13,7 +13,7 @@ public class ShootVomit : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space))
         {
-            Rigidbody newVomit = Instantiate(vomit, new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z + 2f), Quaternion.identity) as Rigidbody;
+            Rigidbody newVomit = Instantiate(vomit, new Vector3(transform.position.x, transform.position.y + 3f, transform.position.z + 2f), Quaternion.identity) as Rigidbody;
             newVomit.AddForce(Vector3.forward * 100f);
         }
 	}
